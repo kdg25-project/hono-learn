6fc97e7f80b8a7e7a47474e777f746cca08257a9
tah5882 <me@tah5882.dev>
2026-05-14 14:59:03 +0900

    feat: integrate Drizzle ORM with SQLite and restructure routes to a new directory

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Hono } from "hono";
-import userRoute from "./user";
+import { Hono } from 'hono';
+import userRoute from './routes/user';
 
 const app = new Hono();
 
-const api = new Hono().route("/user", userRoute);
+const api = new Hono().route('/user', userRoute);
 
-const _routes = app.route("/api", api);
+const _routes = app.route('/api', api);
 
 export default {
   fetch: app.fetch,
