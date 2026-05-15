9ecfbcd3d59cb1601d34079c9ca651120bb655d6
tah5882 <me@tah5882.dev>
2026-05-15 16:51:28 +0900

    feat: corsを追加

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import { Hono } from 'hono';
 import userRoute from './routes/user';
 import { bearerAuth } from 'hono/bearer-auth';
+import { cors } from 'hono/cors';
 
 const token = 'honoiscool';
 
 const app = new Hono();
 app.use('/api/*', bearerAuth({ token }));
+app.use(
+  '*',
+  cors({
+    origin: ['http://localhost:3000'],
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowHeaders: ['Content-Type'],
+    exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+    credentials: true,
+  })
+);
 
 const api = new Hono().route('/user', userRoute);
 
